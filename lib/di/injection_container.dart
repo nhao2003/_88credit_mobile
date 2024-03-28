38cadb2cddb4edc5b3be5e42b00e8726f47eec5d
minhphan46 <21522345@gm.uit.edu.vn>
2024-03-28 22:54:50 +0700

    add di

---
+++ lib/di/injection_container.dart
@@ -0,0 +1,9 @@
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+
+final sl = GetIt.instance;
+
+Future<void> initializeDependencies() async {
+  // Dio
+  sl.registerSingleton<Dio>(Dio());
+}
