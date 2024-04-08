6a470bed97d4e64fb48e0710476830080249bf11
minhphan46 <21522345@gm.uit.edu.vn>
2024-04-08 21:40:10 +0700

    Refactor ApprovedRequestTab widget

--- lib/features/presentation/modules/contract/request/widgets/approved_request_tab.dart
+++ lib/features/presentation/modules/contract/request/widgets/approved_request_tab.dart
@@ -1,51 +1,11 @@
-import 'package:_88credit_mobile/features/domain/entities/loan_request.dart';
-import 'package:_88credit_mobile/features/presentation/modules/contract/bloc/request/request_bloc.dart';
-import 'package:_88credit_mobile/features/presentation/modules/contract/request/widgets/request_item.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import '../../../../../../config/theme/app_color.dart';
-import '../../../../../../config/theme/text_styles.dart';
+import '../../../../../domain/entities/loan_request.dart';
+import '../../bloc/request/request_bloc.dart';
+import 'base_list_request.dart';
+import 'request_item.dart';
 
-class ApprovedRequestTab extends StatefulWidget {
+class ApprovedRequestTab extends StatelessWidget {
   const ApprovedRequestTab({super.key});
-
-  @override
-  State<ApprovedRequestTab> createState() => _ApprovedRequestTabState();
-}
-
-class _ApprovedRequestTabState extends State<ApprovedRequestTab> {
-  final scrollController = ScrollController();
-  final String titleNull = "Chưa có yêu cầu đã đăng";
-
-  @override
-  void initState() {
-    refresh();
-    scrollController.addListener(() {
-      if (scrollController.position.maxScrollExtent ==
-          scrollController.offset) {
-        fetchMore();
-      }
-    });
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    scrollController.dispose();
-    super.dispose();
-  }
-
-  Future fetchMore() async {
-    context
-        .read<RequestBloc>()
-        .add(const FetchMoreRequestEvent(RequestStatusTypes.approved));
-  }
-
-  Future refresh() async {
-    context
-        .read<RequestBloc>()
-        .add(const RefreshRequestEvent(RequestStatusTypes.approved));
-  }
 
   Widget? buildItem(LoanRequestEntity request) {
     return RequestItem(request: request);
@@ -53,63 +13,11 @@ class _ApprovedRequestTabState extends State<ApprovedRequestTab> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: RefreshIndicator(
-        onRefresh: refresh,
-        child: Column(
-          children: [
-            Expanded(
-              child: BlocBuilder<RequestBloc, RequestState>(
-                builder: (context, state) {
-                  switch (state.status) {
-                    case RequestFetchStatus.loading:
-                      return const Center(
-                        child: CircularProgressIndicator(),
-                      );
-                    case RequestFetchStatus.success:
-                      if (state.requestsApproved.isEmpty) {
-                        return Stack(
-                          children: <Widget>[
-                            ListView(),
-                            Center(
-                              child: Text(
-                                titleNull,
-                                style: AppTextStyles.bold20
-                                    .copyWith(color: AppColors.green),
-                              ),
-                            ),
-                          ],
-                        );
-                      }
-                      return ListView.builder(
-                        controller: scrollController,
-                        itemCount: state.requestsApproved.length + 1,
-                        scrollDirection: Axis.vertical,
-                        shrinkWrap: true,
-                        itemBuilder: (context, index) {
-                          if (index < state.requestsApproved.length) {
-                            return buildItem(state.requestsApproved[index]);
-                          } else {
-                            return state.hasMore
-                                ? const Padding(
-                                    padding: EdgeInsets.all(12.0),
-                                    child: Center(
-                                      child: CircularProgressIndicator(),
-                                    ),
-                                  )
-                                : const SizedBox(height: 20);
-                          }
-                        },
-                      );
-                    default:
-                      return const SizedBox();
-                  }
-                },
-              ),
-            ),
-          ],
-        ),
+    return const Padding(
+      padding: EdgeInsets.all(8.0),
+      child: BaseListRequests(
+        titleNull: "Chưa có tin đã đăng",
+        requestType: RequestStatusTypes.approved,
       ),
     );
   }
---
+++ lib/features/presentation/modules/contract/request/widgets/base_list_request.dart
@@ -0,0 +1,135 @@
+import 'package:_88credit_mobile/features/domain/entities/loan_request.dart';
+import 'package:_88credit_mobile/features/presentation/modules/contract/bloc/request/request_bloc.dart';
+import 'package:_88credit_mobile/features/presentation/modules/contract/request/widgets/request_item.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../../../../../../config/theme/app_color.dart';
+import '../../../../../../config/theme/text_styles.dart';
+
+class BaseListRequests extends StatefulWidget {
+  final RequestStatusTypes requestType;
+  final String titleNull;
+  const BaseListRequests({
+    required this.requestType,
+    required this.titleNull,
+    super.key,
+  });
+
+  @override
+  State<BaseListRequests> createState() => _BaseListRequestsState();
+}
+
+class _BaseListRequestsState extends State<BaseListRequests> {
+  final scrollController = ScrollController();
+
+  @override
+  void initState() {
+    refresh();
+    scrollController.addListener(() {
+      if (scrollController.position.maxScrollExtent ==
+          scrollController.offset) {
+        fetchMore();
+      }
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    scrollController.dispose();
+    super.dispose();
+  }
+
+  Future fetchMore() async {
+    context.read<RequestBloc>().add(FetchMoreRequestEvent(widget.requestType));
+  }
+
+  Future refresh() async {
+    context.read<RequestBloc>().add(RefreshRequestEvent(widget.requestType));
+  }
+
+  Widget? buildItem(LoanRequestEntity request) {
+    return RequestItem(request: request);
+  }
+
+  List<LoanRequestEntity> getRequests(RequestState state) {
+    switch (widget.requestType) {
+      case RequestStatusTypes.approved:
+        return state.requestsApproved;
+      case RequestStatusTypes.pending:
+        return state.requestsPending;
+      case RequestStatusTypes.rejected:
+        return state.requestsApproved;
+      case RequestStatusTypes.sent:
+        return state.requestsSent;
+      case RequestStatusTypes.waitingPayment:
+        return state.requestsWaitingPayment;
+      default:
+        return [];
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: RefreshIndicator(
+        onRefresh: refresh,
+        child: Column(
+          children: [
+            Expanded(
+              child: BlocBuilder<RequestBloc, RequestState>(
+                builder: (context, state) {
+                  List<LoanRequestEntity> requests = getRequests(state);
+                  switch (state.status) {
+                    case RequestFetchStatus.loading:
+                      return const Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    case RequestFetchStatus.success:
+                      if (requests.isEmpty) {
+                        return Stack(
+                          children: <Widget>[
+                            ListView(),
+                            Center(
+                              child: Text(
+                                widget.titleNull,
+                                style: AppTextStyles.bold20
+                                    .copyWith(color: AppColors.green),
+                              ),
+                            ),
+                          ],
+                        );
+                      }
+                      return ListView.builder(
+                        controller: scrollController,
+                        itemCount: requests.length + 1,
+                        scrollDirection: Axis.vertical,
+                        shrinkWrap: true,
+                        itemBuilder: (context, index) {
+                          if (index < requests.length) {
+                            return buildItem(requests[index]);
+                          } else {
+                            return state.hasMore
+                                ? const Padding(
+                                    padding: EdgeInsets.all(12.0),
+                                    child: Center(
+                                      child: CircularProgressIndicator(),
+                                    ),
+                                  )
+                                : const SizedBox(height: 20);
+                          }
+                        },
+                      );
+                    default:
+                      return const SizedBox();
+                  }
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
