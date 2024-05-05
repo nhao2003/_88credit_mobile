e6c8aacf13355e16a65a78dc5d4b0f1220bf8a8b
minhphan46 <21522345@gm.uit.edu.vn>
2024-05-06 00:30:04 +0700

    generate QR

--- lib/features/presentation/modules/qr_code/screens/generate_code_screen.dart
+++ lib/features/presentation/modules/qr_code/screens/generate_code_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pretty_qr_code/pretty_qr_code.dart';
 
 class GenerateCodeScreen extends StatefulWidget {
   const GenerateCodeScreen({super.key});
@@ -9,19 +10,32 @@ class GenerateCodeScreen extends StatefulWidget {
 
 class _GenerateCodeScreenState extends State<GenerateCodeScreen> {
   String? qrData;
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Generate QR Code'),
       ),
-      body: const Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('Generate QR Code'),
-          ],
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            children: [
+              TextField(
+                decoration: const InputDecoration(
+                  hintText: 'Enter data to encode',
+                ),
+                onSubmitted: (value) {
+                  setState(() {
+                    qrData = value;
+                  });
+                },
+              ),
+              const SizedBox(height: 20),
+              if (qrData != null) PrettyQrView.data(data: qrData!),
+            ],
+          ),
         ),
       ),
     );
