9eddae5f08db94020a0083f5a96809bf05d13a74
minhphan46 <21522345@gm.uit.edu.vn>
2024-05-13 22:05:12 +0700

    ScanCodeScreen

--- lib/features/presentation/modules/qr_code/screens/scan_code_screen.dart
+++ lib/features/presentation/modules/qr_code/screens/scan_code_screen.dart
@@ -2,6 +2,7 @@ import 'dart:typed_data';
 import 'package:_88credit_mobile/features/presentation/globalwidgets/my_appbar.dart';
 import 'package:flutter/material.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
+import '../widgets/qr_scanner_qverlay.dart';
 
 class ScanCodeScreen extends StatelessWidget {
   const ScanCodeScreen({super.key});
@@ -10,34 +11,26 @@ class ScanCodeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const MyAppbar(title: 'Quét mã QR'),
-      body: MobileScanner(
-        controller: MobileScannerController(
-          detectionSpeed: DetectionSpeed.noDuplicates,
-        ),
-        onDetect: (capture) {
-          final List<Barcode> barcodes = capture.barcodes;
-          final Uint8List? image = capture.image;
-          // for (final barcode in barcodes) {
-          //   print("Barcode found: ${barcode.rawValue}");
-          // }
-          if (barcodes.isNotEmpty) {
-            showDialog(
-              context: context,
-              builder: (context) => AlertDialog(
-                title: Text(barcodes.first.rawValue ?? "No barcode found"),
-              ),
-            );
-          }
-          if (image != null) {
-            showDialog(
-              context: context,
-              builder: (context) => AlertDialog(
-                title: Text(barcodes.first.rawValue ?? "No barcode found"),
-                content: Image.memory(image),
-              ),
-            );
-          }
-        },
+      body: Stack(
+        children: [
+          MobileScanner(
+            controller: MobileScannerController(
+              detectionSpeed: DetectionSpeed.noDuplicates,
+            ),
+            onDetect: (capture) {
+              final List<Barcode> barcodes = capture.barcodes;
+              if (barcodes.isNotEmpty) {
+                showDialog(
+                  context: context,
+                  builder: (context) => AlertDialog(
+                    title: Text(barcodes.first.rawValue ?? "No barcode found"),
+                  ),
+                );
+              }
+            },
+          ),
+          QRScannerOverlay(overlayColour: Colors.black.withOpacity(0.5)),
+        ],
       ),
     );
   }
