82126a4960d280225c5db0a0e2fe2bc9368115f0
minhphan46 <21522345@gm.uit.edu.vn>
2024-06-02 17:06:38 +0700

    verification

---
+++ lib/features/presentation/modules/verification/screens/verification_portrait_screen.dart
@@ -0,0 +1,92 @@
+// import 'package:flutter/material.dart';
+// import 'package:get/get.dart';
+// import 'package:nhagiare_mobile/core/extensions/integer_ex.dart';
+// import 'package:nhagiare_mobile/core/extensions/textstyle_ex.dart';
+
+// import '../../../../../config/theme/app_color.dart';
+// import '../../../../../config/theme/text_styles.dart';
+// import '../verification_controller.dart';
+// import '../widgets/choose_image_portrait.dart';
+// import '../widgets/stepper_identify.dart';
+
+// class VerificationPortraitScreen extends StatelessWidget {
+//   VerificationPortraitScreen({super.key});
+//   final VerificationController _controller = Get.find<VerificationController>();
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: AppColors.backgroundColor,
+//       appBar: AppBar(
+//         title: const Text("Chụp ảnh chân dung"),
+//       ),
+//       // body
+//       body: Column(
+//         children: [
+// // container steps
+//           Container(
+//             decoration: BoxDecoration(
+//               color: AppColors.white,
+//               borderRadius: BorderRadius.circular(10),
+//             ),
+//             margin: const EdgeInsets.all(10),
+//             padding:
+//                 const EdgeInsets.only(left: 20, right: 20, top: 20, bottom: 10),
+//             child: Column(
+//               children: [
+//                 Text(
+//                   "Vui lòng cung cấp cho chúng tôi hình ảnh thật chân dung của bạn.",
+//                   textAlign: TextAlign.center,
+//                   style: AppTextStyles.bold14,
+//                 ),
+//                 const SizedBox(height: 10),
+//                 const StepperIdentify(1),
+//               ],
+//             ),
+//           ),
+// // change type identity documents
+//           Container(
+//             decoration: BoxDecoration(
+//               color: AppColors.white,
+//               borderRadius: BorderRadius.circular(10),
+//             ),
+//             margin: const EdgeInsets.symmetric(horizontal: 10),
+//             padding: const EdgeInsets.symmetric(
+//               vertical: 20,
+//               horizontal: 20,
+//             ),
+//             width: double.infinity,
+//             child: ChooseImagePortrait(_controller),
+//           ),
+//           const Expanded(child: SizedBox()),
+//           Padding(
+//             padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+//             child: Obx(
+//               () => ElevatedButton(
+//                 onPressed: _controller.isCanClickPortrait.value
+//                     ? () {
+//                         _controller.navToInforScreen();
+//                       }
+//                     : null,
+//                 style: ElevatedButton.styleFrom(
+//                   backgroundColor: AppColors.green,
+//                   padding: const EdgeInsets.symmetric(vertical: 15),
+//                   textStyle: const TextStyle(color: AppColors.white),
+//                   elevation: 10,
+//                   minimumSize: Size(100.wp, 55),
+//                   shape: RoundedRectangleBorder(
+//                     borderRadius: BorderRadius.circular(10),
+//                   ),
+//                 ),
+//                 child: Text(
+//                   'Tiếp tục'.tr,
+//                   style: AppTextStyles.bold14.colorEx(AppColors.white),
+//                 ),
+//               ),
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
